3f7a6b1291194c02afb2edd0a2200ecdeccafe66
Reend <reend21@protonmail.com>
2026-06-25 00:48:06 +0300

    about page and little home page improvements

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,6 +1,18 @@
 let downloadSettingsShow = document.querySelector(".download-alt")
 let downloadBTN = document.querySelector(".download")
+let networkLimitInput = document.getElementById("network-limit")
+let networkLimitVal = document.getElementById("network-limit-val")
 
 downloadBTN.addEventListener("click", () => {
     downloadSettingsShow.setAttribute("id", "show")
 })
+
+if (networkLimitInput && networkLimitVal) {
+    networkLimitInput.addEventListener("input", (e) => {
+        networkLimitVal.textContent = e.target.value + " Mbps"
+    })
+} // if (networkLimitInput && networkLimitVal) {
+     // networkLimitInput.addEventListener("input", (e) => {
+    // networkLimitVal.textContent = 0 + " dont limit"
+    // })
+// }
